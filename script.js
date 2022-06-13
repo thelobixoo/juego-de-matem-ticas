95f5094ae09b92e814853498ef9fe37b5060b4b6
thelobixoo <98877668+thelobixoo@users.noreply.github.com>
2022-06-13 06:15:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,90 +25,90 @@ function nuevaSuma() {
    
     n1 = parseInt(Math.random() * 10);
     n2 = parseInt(Math.random() * 10);
-    //asignamos los numeros a las etiquetas
+    
     num1.innerHTML = n1;
     num2.innerHTML = n2;
-    //colocamos el curso en el input
+    
     respuesta_usuario.focus();
 }
 
 //Funcion producto
 function btnProducto() {
-    //limpiamos el div contenedor de las correcciones
+    
     msj_correccion.innerHTML = "";
-    //agregamos la clase activa al boton producto y la quitamos del resto
+    
     activarBoton("producto");
     operacion_actual = "*";
-    //asignamos la operacion suma a la etiqueta
+   
     operacion.innerHTML = " x ";
-    //generamos los numeros aletarios de la suma
+    
     nuevoProducto();
 }
 
 function nuevoProducto() {
-    //generamos dos numeros aletarios entre 0 y 9
+    
     n1 = parseInt(Math.random() * 10);
     n2 = parseInt(Math.random() * 10);
-    //asignamos los numeros a las etiquetas
+    
     num1.innerHTML = n1;
     num2.innerHTML = n2;
-    //colocamos el curso en el input
+    
     respuesta_usuario.focus();
 }
 
 //funcion resta
 function btnResta() {
-    //limpiamos el div contenedor de las correcciones
+  
     msj_correccion.innerHTML = "";
-    //agregamos la clase activa al boton suma y la quitamos del resto
+   
     activarBoton("resta");
     operacion_actual = "-";
-    //asignamos la operacion suma a la etiqueta
+    
     operacion.innerHTML = " - ";
-    //generamos los numeros aletarios de la suma
+    
     nuevaResta();
 }
 
 function nuevaResta() {
-    //generamos dun numeros aletarios entre 5 y 10
+   
     n1 = parseInt(Math.random() * 5 + 5);
-    //generamos un numero aleatorio entre 0 y 5
+    
     n2 = parseInt(Math.random() * 5);
-    //asignamos los numeros a las etiquetas
+    
     num1.innerHTML = n1;
     num2.innerHTML = n2;
-    //colocamos el curso en el input
+   
     respuesta_usuario.focus();
 }
 
 //funcion división
 function btnDivision() {
-    //limpiamos el div contenedor de las correcciones
+   
     msj_correccion.innerHTML = "";
-    //agregamos la clase activa al boton suma y la quitamos del resto
+    
     activarBoton("division");
     operacion_actual = "/";
-    //asignamos la operacion suma a la etiqueta
+    
     operacion.innerHTML = " / ";
-    //generamos los numeros aletarios de la suma
+    
     nuevaDivision();
 }
 
 function nuevaDivision() {
-    //aqui voy a guardar los divisores del numero a dividr
+    
     let divisores = [];
 
-    //generamos un numero aletorio entre 1 y 10
+    
     n1 = parseInt(Math.random() * 9 + 1);
 
-    //encontramos los divisores del numero generado y lo guardamos en el arreglo
+    
     for (var i = 1; i <= n1; i++) {
-        if (n1 % i === 0) { //es divisor
+        if (n1 % i === 0) { 
             divisores.push(i);
         }
     }
 
-    //seleccionamos un posiciòn aleatorio de los numeros que son divisores
+    
     let pos = parseInt(Math.random() * (divisores.length));
 
     n2 = divisores[pos];
@@ -117,32 +117,31 @@ function nuevaDivision() {
     respuesta_usuario.focus();
 }
 
-//funcion que controla si la respuesta es correcta
+
 function corregir() {
-    //si el usuario no ha ingresado nada no continuo
+    
     if (respuesta_usuario.value == "") {
         return;
     }
 
     let solucion;
-    //armo la operacion que se genero en una variable y veo cual es su reslutado
-    //En este caso el operador + es para concatener las cadenas
+    
     let operacion = n1 + operacion_actual + n2;
     solucion = eval(operacion);
 
-    //creo un elemento i para agregar el icono de correcto o incorrecto
+    
     var i = document.createElement("i");
-    //controlo si coincide lo que el usuario respondio con la solucion
+    
     if (respuesta_usuario.value == solucion) {
         i.className = "fa-regular fa-face-grin";
     } else {
         i.className = "fa-regular fa-face-frown";
     }
 
-    //agrego el elemento al contenedor de las correciones
+    
     msj_correccion.appendChild(i);
 
-    //controlo que tipo de operacion estoy para genera una nueva operacion
+   
     if (operacion_actual == "+") {
         nuevaSuma();
     } else if (operacion_actual == "-") {
@@ -153,12 +152,11 @@ function corregir() {
         nuevaDivision();
     }
 
-    //limpio el input
+    
     respuesta_usuario.value = "";
 }
 
-//agrego al input el evento onkeydown para detectar cuando se presiona Enter Y 
-//llamar directamente a la funcion corregir()
+
 respuesta_usuario.onkeydown = function(e) {
     var ev = document.all ? window.event : e;
     if (ev.keyCode == 13) {
@@ -167,7 +165,7 @@ respuesta_usuario.onkeydown = function(e) {
 }
 
 
-//Esta funcion la creamos luego, cuando tengamos listo los estilos
+
 function activarBoton(idBoton) {
     document.getElementById("suma").className = "";
     document.getElementById("resta").className = "";
